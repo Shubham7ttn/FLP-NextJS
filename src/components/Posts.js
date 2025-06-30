2d0f6faf93399d90efdb31714830c6a182bab3d4
Shubham <shubham@tothenew.com>
2025-06-30 19:13:35 +0530

    FLP - Assignment 1

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,7 +4,7 @@ export default async function Posts() {
     const data = await fetchPostsData();
     return (
         <div className = "post-container">
-            <div className = "title">3. Time based revalidation - Cached data will be revalidated after every 60 seconds</div>
+            <div className = "title">3. Time based revalidation - Cached data will be revalidated after every 60 seconds - Posts fetch API by id</div>
             <div className="card">
                 <img src={data?.image} alt="Post Image" />
                 <div className="card-title">{data?.title}</div>
